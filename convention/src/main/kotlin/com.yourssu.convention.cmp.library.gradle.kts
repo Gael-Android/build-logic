ff2caf8668a5a06833ecbfad8189b127260a6379
kwakkun2002 <gael.urssu@gmail.com>
2026-04-05 00:51:16 +0900

    chore: remove compose material icon core dependency

--- convention/src/main/kotlin/com.yourssu.convention.cmp.library.gradle.kts
+++ convention/src/main/kotlin/com.yourssu.convention.cmp.library.gradle.kts
@@ -11,7 +11,6 @@ dependencies {
     commonMainImplementation(library("compose-ui"))
     commonMainImplementation(library("compose-foundation"))
     commonMainImplementation(library("compose-material3"))
-    commonMainImplementation(library("jetbrains-compose-material-icons-core"))
 
     // CMP 1.10.0+: Resources and preview tooling are now separate modules
     commonMainImplementation(library("compose-components-resources"))
